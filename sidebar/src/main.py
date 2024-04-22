baa6e61d2a4359ffed98c36e29bda501ed0e7730
Carlos Sigua <carlos.sigua@gmail.com>
2024-04-22 01:06:48 -0500

    creando sidebar

--- sidebar/src/main.py
+++ sidebar/src/main.py
@@ -5,26 +5,51 @@ class App(UserControl):
         super().__init__()
 
         self.pg = pg
+        self.pg.window_title_bar_buttons_hidden = True
+        self.pg.window_title_bar_hidden = True
         self.init_helper()
 
     def init_helper(self):
         self.pg.add(
             Container(
-                expand=True,
+                expand=True, padding=1,
                 bgcolor="#2C2C2C",
+                border_radius=15,
                 content=Row(
                     spacing=0,
                     controls=[
                         Container(
-                            bgcolor="#E68745",
+                            bgcolor="#006262",
                             # expand=True,
                             width=80,
+                            content=Row(
+                                spacing=0,
+                                controls=[
+                                    Column(
+                                        expand=True,
+                                        controls=[
+                                            Container(bgcolor="#006262",
+                                                      expand=True,)
+                                        ]
+                                    ),
+                                    Column(
+                                        expand=True,
+                                        controls=[
+                                            Container(bgcolor="red",
+                                                      width=3,height=10,)
+                                        ]
+                                    )
+                                ]
+                            )
                         ), #sidebar
                         Container(
-                            bgcolor="#E05C4A",
-                            # expand=True
+                            bgcolor="#fdfcff",
+                            expand=True,
+                            padding=0,
+                            margin=0,
                         ), #main screen
-                    ]
+                    ],
+                    vertical_alignment=10,
                 )
             )
         )
