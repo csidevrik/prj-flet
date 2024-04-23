e88672977e52e52cc475aecf31cdef967b32dc3f
Carlos Sigua <carlos.sigua@gmail.com>
2024-04-22 21:25:16 -0500

    agregando favicon

--- sidebar/src/main.py
+++ sidebar/src/main.py
@@ -5,8 +5,9 @@ class App(UserControl):
         super().__init__()
 
         self.pg = pg
-        self.pg.window_title_bar_buttons_hidden = True
-        self.pg.window_title_bar_hidden = True
+        # self.pg.window_title_bar_buttons_hidden = True
+        # self.pg.window_title_bar_hidden = True
+        self.pg.title="PAYMENTS"
         self.init_helper()
 
     def init_helper(self):
@@ -14,13 +15,11 @@ class App(UserControl):
             Container(
                 expand=True, padding=1,
                 bgcolor="#2C2C2C",
-                border_radius=15,
                 content=Row(
                     spacing=0,
                     controls=[
                         Container(
-                            bgcolor="#006262",
-                            # expand=True,
+                            bgcolor="#008888",
                             width=80,
                             content=Row(
                                 spacing=0,
@@ -28,29 +27,34 @@ class App(UserControl):
                                     Column(
                                         expand=True,
                                         controls=[
-                                            Container(bgcolor="#006262",
-                                                      expand=True,)
+                                            Container(bgcolor="#db9d30",
+                                                      width=100,height=100,)
                                         ]
                                     ),
+                                    # colum-icon
                                     Column(
                                         expand=True,
                                         controls=[
                                             Container(bgcolor="red",
-                                                      width=3,height=10,)
+                                                      width=3,height=100,)
                                         ]
                                     )
-                                ]
+                                    # colum line
+                                ],
                             )
-                        ), #sidebar
+                        ), 
+                        #sidebar
                         Container(
                             bgcolor="#fdfcff",
                             expand=True,
                             padding=0,
                             margin=0,
-                        ), #main screen
+                        ), 
+                        #main screen
                     ],
                     vertical_alignment=10,
                 )
             )
         )
-app(target=App)
+# app(port=3000,target=App, assets_dir="assets", view=AppView.WEB_BROWSER)
+app(target=App, assets_dir="assets")
