a2c745b26d200d0293436efa189401e48e740e83
carlos s <carlos.sigua@gmail.com>
2024-04-25 17:28:00 -0500

    cambios en main

--- app/src/test.py
+++ app/src/test.py
@@ -1,46 +1,30 @@
 import flet as ft
 
-class MyApp(ft.FletApp):
-    def build(self):
-        # Crear la AppBar
-        appbar = ft.AppBar(title="Mi AppBar")
-        
-        # Crear un GestureDetector para detectar eventos de mouse
-        gesture_detector = ft.GestureDetector(
-            on_tap_down=self.on_tap_down,
-            on_tap_up=self.on_tap_up,
-            on_double_tap=self.on_double_tap,
-            on_long_press=self.on_long_press,
-            on_pan_start=self.on_pan_start,
-            on_pan_update=self.on_pan_update,
-            on_pan_end=self.on_pan_end,
-        )
-        
-        # Añadir el GestureDetector a la AppBar
-        appbar.add_widget(gesture_detector)
-        
-        return appbar
+def main(page: ft.Page):
+    def check_item_clicked(e):
+        e.control.checked = not e.control.checked
+        page.update()
 
-    def on_tap_down(self, event):
-        print("Tap down:", event)
+    page.appbar = ft.AppBar(
+        leading=ft.Icon(ft.icons.PALETTE),
+        leading_width=40,
+        title=ft.Text("AppBar Example"),
+        center_title=False,
+        bgcolor=ft.colors.SURFACE_VARIANT,
+        actions=[
+            ft.IconButton(ft.icons.WB_SUNNY_OUTLINED),
+            ft.IconButton(ft.icons.FILTER_3),
+            ft.PopupMenuButton(
+                items=[
+                    ft.PopupMenuItem(text="Item 1"),
+                    ft.PopupMenuItem(),  # divider
+                    ft.PopupMenuItem(
+                        text="Checked item", checked=False, on_click=check_item_clicked
+                    ),
+                ]
+            ),
+        ],
+    )
+    page.add(ft.Text("Body!"))
 
-    def on_tap_up(self, event):
-        print("Tap up:", event)
-
-    def on_double_tap(self, event):
-        print("Double tap:", event)
-
-    def on_long_press(self, event):
-        print("Long press:", event)
-
-    def on_pan_start(self, event):
-        print("Pan start:", event)
-
-    def on_pan_update(self, event):
-        print("Pan update:", event)
-
-    def on_pan_end(self, event):
-        print("Pan end:", event)
-
-if __name__ == '__main__':
-    MyApp.run()
+ft.app(target=main)
