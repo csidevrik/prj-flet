de62aeaf114c06b2f6218aea5c84a96662513667
Carlos Sigua <carlos.sigua@gmail.com>
2024-05-20 01:46:36 -0500

    mejorando la interfaz y eliminando el lib

--- app/src/main.py
+++ app/src/main.py
@@ -17,7 +17,7 @@ COLOR2="#222222"
 GRADIENT=ft.LinearGradient(
     begin=ft.alignment.top_left,
     end=ft.Alignment(0.8,1),
-    colors = gradient("Pun Yeta"),
+    colors = gradient("Kye Meh"),
     rotation=math.pi/4.6,
 )
 
