ba9e153c5056d76aac389ecb0a29a958829e4d15
Carlos Sigua <carlos.sigua@gmail.com>
2024-08-17 17:50:38 -0500

    vacations py

--- app/src/li/vacations.py
+++ app/src/li/vacations.py
@@ -3,4 +3,11 @@ from datetime import datetime, timedelta
 class Vacations:
     def __init__(self, start_date):
         self.start_date = datetime.strptime
-        pass
+    
+    def calc_initdayper01( date_str  ):  
+        return 0
+    def currentdate():
+        return datetime.today()
+
+
+print(Vacations.currentdate())
